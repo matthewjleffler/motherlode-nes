1b714624b5d97ca122b5cba8fdb7672b5fd3c499
Matthew Leffler <matthewjleffler@gmail.com>
2020-09-06 09:52:12 -0700

    Reorg and cleanup

--- vars.asm
+++ vars.asm
@@ -1,13 +1,17 @@
-; VARIABLES
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Variables
+  .rsset $0000                ; put pointers in zero page
 
-  .rsset $0000       ; put pointers in zero page
+pointerLo   .rs 1             ; pointer variables are declared in RAM
+pointerHi   .rs 1             ; low byte first, high byte immediately after
+buttons     .rs 1             ; buttons
 
-pointerLo   .rs 1   ; pointer variables are declared in RAM
-pointerHi   .rs 1   ; low byte first, high byte immediately after
-buttons     .rs 1   ; buttons
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Constants
 
-; CONSTANTS
+; Controllers
 CONTROLLER1 = $4016
+CONTROLLER2 = $4017
 BUTTONA     = %10000000
 BUTTONB     = %01000000
 BUTTONSEL   = %00100000
@@ -18,5 +22,5 @@ BUTTONL     = %00000010
 BUTTONR     = %00000001
 
 ; Move Speed
-SLOW        = $01
-FAST        = $03
+SLOW        = $01             ; 1 pixel per frame
+FAST        = $03             ; 3 pixels per frame
